dfbe97e8e5c5e96abf21e35e8f68672e2d0454b9
pascalheude <pascalheude@hotmail.com>
2021-05-01 20:06:01 +0200

    Call initialization of esc.cpp

--- data_ini.cpp
+++ data_ini.cpp
@@ -3,21 +3,23 @@
 #include "data_ini.h"
 #include "battery.h"
 #include "drone.h"
+#include "esc.h"
 #include "gps.h"
 #include "led.h"
 #include "mode.h"
 #include "mpu6050.h"
-#include "pid.h"
 #include "nrf24l01.h"
+#include "pid.h"
 
 void initializeData(void)
 {
     initializeBattery();
     initializeDrone();
+    initializeEsc();
     initializeGps();
     initializeLed();
     initializeMode();
     initializeMpu6050();
-    initializePid();
     initializeRadio();
+    initializePid();
 }
